4d24679f33b567fdcd28014021c5e247612bcc9d
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-11-25 00:32:49 +0000

    sql statements to delete volunteers inserted automatically

---
+++ backend/src/main/resources/db/migration/postgresql/V1_0_18_Delete_Volunteers_Automatically_Inserted.sql
@@ -0,0 +1,11 @@
+delete from credential_roles where credential_id = 10 and role_id = 2;
+delete from credential_roles where credential_id = 11 and role_id = 2;
+
+delete from volunteers where id = '932362279f2aeb3674521e602e117a1d';
+delete from volunteers where id = '42c58abd933c11304fcaa7a18cefaaaa';
+
+delete from credentials where id = '9c7860f1-3712-4a08-a757-3b2c543bc7g3';
+delete from credentials where id = '932362279f2aeb3674521e602e117a1d';
+
+delete from email_confirmation where id = 10;
+delete from email_confirmation where id = 11;
--- backend/src/main/resources/db/migration/postgresql/V1_0_19_Populate_With_Volunteers.sql
+++ backend/src/main/resources/db/migration/postgresql/V1_0_19_Populate_With_Volunteers.sql
@@ -40,6 +40,48 @@ VALUES
         null,
         null,
         '9c7860f1-3712-4a08-a757-3b2c543bc7g3'
-    )
+    );
 -------------------------------------------------
 --2
+INSERT INTO email_confirmation
+(id,email, hash, created_on, updated_on)
+VALUES
+    (
+        11,
+        'fulanita@gmail.com',
+        '932362279f2aeb3674521e602e117a1d',
+        '2022-11-24 23:50:52.274000',
+        '2022-11-24 23:50:52.274000'
+    );
+
+INSERT INTO credentials
+(email, hashed_password, email_confirmed,
+ email_confirmation_id, hash_recovery_password,
+ created_recovery_hash_on, id, surrogate_key)
+VALUES
+    ('fulanita@gmail.com',
+     '$2a$10$gNWZ8j0zre/0zHbql.QoEeUpWZpbVi5dD7s7xjfDKEOZbsJJ23vbK',
+     true,
+     11,
+     NULL,
+     NULL,
+     '932362279f2aeb3674521e602e117a1d',
+     11
+    );
+
+INSERT INTO credential_roles
+(credential_id, role_id)
+VALUES
+    (11,2);
+
+INSERT INTO volunteers
+(id, surrogate_key, location_id,
+ profile_id, credential_id)
+VALUES
+    (
+        '932362279f2aeb3674521e602e117a1d',
+        11,
+        null,
+        null,
+        '932362279f2aeb3674521e602e117a1d'
+    );
